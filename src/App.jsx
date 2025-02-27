5159c4313961ff39fb1587a0a0c11cce5e8571b5
Gopal-student21 <chaudharygopal392@gmail.com>
2025-02-27 10:07:08 +0545

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 import Button from './Components/Button'
+import Header from './Components/Header'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Navbar/>
-   
+    <Header/>
+ 
     </>
   )
 }
